6c61eaedfdb53801e17752ecac3d018f8ab3ed5a
Pragyanam Tiwari <pragyanamtiwarii@gmail.com>
2025-07-23 21:26:24 +0530

    loops

---
+++ 01 pattern/one.js
@@ -0,0 +1,21 @@
+// Stirng +String = concatinaiton
+// String + int = string concatination
+// int + int = int arthmatic operation
+
+let c = "Hello"
+let d = " world..!"
+console.log(typeof (c + d))
+
+// let age = Number(prompt(`Enter your age: `))
+// console.log(age)
+
+let a = 5
+let b = 2
+
+// a = a + b
+// b = a - b
+// a = a - b
+
+[a, b] = [b, a]
+
+console.log(`a: ${a} ,b= ${b} `);
---
+++ 02 loops/loop.js
@@ -0,0 +1,34 @@
+// const n = Number(prompt(`Enter the Number: `))
+// let fact = 1
+// for (let i = 1; i <= n; i++) {
+//     fact *= i
+// }
+// console.log(fact)
+
+// let sum = 0
+// for (let i = 0; i <= n; i++) {
+//     sum += i
+// }
+// console.log(sum)
+
+// let evenSum = 0, oddSum = 0
+
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 0) evenSum += i
+//     else oddSum += i
+// }
+
+// console.log(`Even sum: ${evenSum} , Odd sum: ${oddSum}`);
+
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//         console.log(`the factors are: ${i}`)
+//     }
+
+// }
+
+let count = 0;
+for (let i = 2; i <= 20; i + 3) {
+    count++
+}
+console.log(count)
